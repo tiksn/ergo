474a823db9adda63a595a5ab3784a5e711309d5a
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-11-18 17:37:16 -0600

    Redirect to root

--- app/cli.py
+++ app/cli.py
@@ -21,13 +21,13 @@ def main():
         'Fossa': {
             'Web': {
                 'RedirectUrlTemplates': [
-                    'http://*.localhost:4210',
+                    'http://*.localhost:4210/',
                     'http://*.localhost:4210/callback',
-                    'http://*.dev.localhost:4211',
+                    'http://*.dev.localhost:4211/',
                     'http://*.dev.localhost:4211/callback',
-                    'http://*.test.localhost:4210',
+                    'http://*.test.localhost:4210/',
                     'http://*.test.localhost:4210/callback',
-                    'http://*.test.localhost:4211',
+                    'http://*.test.localhost:4211/',
                     'http://*.test.localhost:4211/callback'
                 ],
                 'OriginUrlTemplates': [
